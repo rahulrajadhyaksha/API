67ecacb252c20f0610f5437d92739ed70ca92d08
Rahul Rajadhyaksha <rajadhyaksha.rahul@rtcs-india.in>
2022-03-07 11:28:05 +0530

    Add project files.

---
+++ API/Controllers/ProducerController.cs
@@ -0,0 +1,36 @@
+﻿using Confluent.Kafka;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.Models;
+using Newtonsoft.Json;
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProducerController : ControllerBase
+    {
+
+        private ProducerConfig _config;
+
+        public ProducerController(ProducerConfig config)
+        {
+            _config = config;
+        }
+        
+        [HttpPost("send")]
+        public async Task<ActionResult> Get(string topic, [FromBody] Employee employee)
+        {
+            string serializedEmployee = JsonConvert.SerializeObject(employee);
+            using (var producer=new ProducerBuilder<Null, string>(_config).Build())
+            {
+                await producer.ProduceAsync(topic, new Message<Null, string> { Value = serializedEmployee });
+                producer.Flush(TimeSpan.FromSeconds(10));
+                return Ok(true);
+            }
+        }
+    }
+}
